3902a82cb621c80eb24f8b188c0c68b19464d86a
Petr Los <petrlos@gmail.com>
2022-12-05 15:02:27 +0100

    Day 5: 2 stars with parsing improved

--- 05/05.py
+++ 05/05.py
@@ -3,6 +3,7 @@ import re
 from collections import deque
 def parseData(lines):
     #entry data must be edited - delete line with stack Nr. + empty line
+    #for this function use data.txt
     stackCounter = 9
     instructions = []
     warehouse = [deque() for i in range(stackCounter)]
@@ -17,6 +18,27 @@ def parseData(lines):
                     warehouse[i].append(possibleCrate)
         else:
             instructions.append(list(map(int,regNum.findall(line))))
+    return warehouse, instructions
+
+def parseData_vs2(lines):
+    #for this function use data_2.txt
+    warehouseLines = []
+    instructions = []
+    for line in lines:
+        if "[" in line:
+            warehouseLines.append(line)
+        elif "1" in line and not "move" in line:
+            numbers = regNum.findall(line)
+            numbers = list(map(int, numbers))
+            stackCounter = max(numbers)
+        elif "move" in line:
+            instructions.append(list(map(int, regNum.findall(line))))
+    warehouse = [deque() for _ in range(stackCounter)]
+    for line in warehouseLines:
+        for i in range(stackCounter):
+            possibleCrate = line[1 + i * 4]  # each possible crate is in string on that position
+            if possibleCrate.isalpha():
+                warehouse[i].append(possibleCrate)
     return warehouse, instructions
 
 def performMovesSmallCrane(warehouse, instructions):
@@ -42,15 +64,15 @@ def printFinalState(warehouse):
 
 #MAIN
 regNum = re.compile(r"\d+")
-with open("data.txt") as file:
+with open("data_2.txt") as file:
     lines = file.read().splitlines()
 
 #Task 1
-warehouse, instructions = parseData(lines)
+warehouse, instructions = parseData_vs2(lines) #for data.txt use parseData()
 warehouse = performMovesSmallCrane(warehouse, instructions)
 print("Task 1:", printFinalState(warehouse))
 
 #Task2
-warehouse, instructions = parseData(lines)
+warehouse, instructions = parseData_vs2(lines) #for data.txt use parseData()
 performMovesLargeCrane(warehouse, instructions)
 print("Task 2:", printFinalState(warehouse))
