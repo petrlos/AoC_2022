207ca15ac173751a1fe9eb319815f46d19eefed9
Petr Los <petrlos@gmail.com>
2022-12-25 20:27:54 +0100

    Day 21: 1st star

--- 21/21.py
+++ 21/21.py
@@ -1,25 +1,36 @@
 #Advent of Code 2022: Day 21:
-
+from datetime import datetime
+time_start = datetime.now()
 import re
 def part1(lines):
     monkeys = {}
     done = False
     while not done:
-        for line in lines:
+        for line in lines[:]:
             operation = line.split(": ")
             if operation[1].isnumeric():
                 monkeys[operation[0]] = int(operation[1])
+                index = lines.index(line)
+                lines.pop(index)
             else:
                 monkey1, monkey2 = re_monkey.findall(operation[1])
                 if monkey1 in monkeys.keys() and monkey2 in monkeys.keys():
                     if "+" in operation[1]:
                         monkeys[operation[0]] = monkeys[monkey1] + monkeys[monkey2]
+                        index = lines.index(line)
+                        lines.pop(index)
                     if "-" in operation[1]:
                         monkeys[operation[0]] = monkeys[monkey1] - monkeys[monkey2]
+                        index = lines.index(line)
+                        lines.pop(index)
                     if "*" in operation[1]:
                         monkeys[operation[0]] = monkeys[monkey1] * monkeys[monkey2]
+                        index = lines.index(line)
+                        lines.pop(index)
                     if "/" in operation[1]:
                         monkeys[operation[0]] = monkeys[monkey1] // monkeys[monkey2]
+                        index = lines.index(line)
+                        lines.pop(index)
             if "root" in monkeys.keys():
                 done = True
     return monkeys["root"]
@@ -28,4 +39,4 @@ with open("data.txt") as file:
     lines = file.read().splitlines()
 
 re_monkey = re.compile(r"\w{4}")
-print(part1(lines))
+print("Task 1:",part1(lines))
