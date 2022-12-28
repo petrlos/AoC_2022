e0d9dbfc93ec0c7c1a35209632b63ba727966b5a
Petr Los <petrlos@gmail.com>
2022-12-28 12:10:15 +0100

    Day 9: 1st only, cleaned up

--- 09/09.py
+++ 09/09.py
@@ -18,27 +18,19 @@ def print_grid(visited):
     print(" ")
 
 def perform_move(head, tail, direction, move_head=True):
-    if move_head:
-        new_head = tuple_sum(head, directions[direction])
-    else:
-        new_head = head
-    distance = manh_distance(new_head, tail) #distance between NEW head and OLD tail
-    dx, dy = tuple([(x - y) for x, y in zip(new_head,tail)]) #difference in coords of new head and old tail
-    if distance in [0,1]: #NEW head lies on OLD tail or lies UDLR - dont move
-        new_tail = tail
-    elif distance == 2 and (dx == 0 or dy == 0): #old tail laid UDLR - move same direction as head
-        new_tail = tuple_sum(tail, directions[direction])
+    head = tuple_sum(head, directions[direction])
+    distance = manh_distance(head, tail) #distance between NEW head and OLD tail
+    dx, dy = tuple([(x - y) for x, y in zip(head,tail)]) #difference in coords of new head and old tail
+    if distance == 2 and (dx == 0 or dy == 0): #old tail laid UDLR - move same direction as head
+        tail = tuple_sum(tail, directions[direction])
     elif distance == 2 and (abs(dx) == 1 and abs(dx) == 1): #old tail is diagonal from head - dont move
-        new_tail = tail
+        tail = tail
     elif  distance == 3:
         if (dx,dy) in [(-1,2), (1,2),(-1,-2), (1,-2)]: #UP, DOWN
-            new_tail = tuple_sum(tail, (dx,dy//2))
+            tail = tuple_sum(tail, (dx,dy//2))
         elif (dx,dy) in [(-2,1),(-2,-1),(2,1),(2,-1)]: #LEFT, RIGHT
-            new_tail = tuple_sum(tail, (dx//2,dy))
-    elif distance == 4:
-        if (abs(dx),abs(dy)) == (2,2):
-            new_tail = tuple_sum((dx//2, dy//2), tail)
-    return new_head, new_tail
+            tail = tuple_sum(tail, (dx//2,dy))
+    return head, tail
 
 def perform_task1(instructions):
     visited = set()
@@ -51,24 +43,8 @@ def perform_task1(instructions):
             visited.add(tail)
     return len(visited)
 
-def perform_task2(instructions):
-    visited = set()
-    rope = [(0,0)]*10
-    for instruction in instructions:
-        direction, counter = instruction.split(" ")
-        for _ in range(int(counter)):
-            move_head = True
-            for i in range(len(rope)-1):
-                head, tail = rope[i:i+2]
-                new_head, new_tail = perform_move(head, tail, direction, move_head)
-                rope[i], rope[i+1] = new_head, new_tail
-                move_head = False
-            visited.add(rope[-1])
-    return visited
-
-#TODO: task2 not working yet
 #MAIN
-with open("test_2.txt") as file:
+with open("data.txt") as file:
     instructions = file.read().splitlines()
 
 directions = {"U":(0, 1),"D":(0, -1), "L":(-1, 0),"R":(1, 0)}  # UDLR
@@ -76,8 +52,3 @@ directions = {"U":(0, 1),"D":(0, -1), "L":(-1, 0),"R":(1, 0)}  # UDLR
 #Task1:
 task1_result = perform_task1(instructions)
 print("Task 1:", task1_result)
-
-#Task2:
-task2_result = perform_task2(instructions)
-print_grid(task2_result)
-print("Task 2:", len(task2_result))
