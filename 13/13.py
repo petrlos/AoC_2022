4b0eb838bf9f98b2f04cf82636f83e90eadc52f0
Petr Los <petrlos@gmail.com>
2023-02-01 23:07:28 +0100

    Day 13: parsing done

--- 13/13.py
+++ 13/13.py
@@ -1,36 +1,15 @@
 #Advent of Code 2022: Day 13
-import ast
-def compare_list(left, right):
-    if len(left) > len(right):
-        return False
-    elif len(left) < len(right):
-        return True
-    else:
-        for item1, item2 in zip(left, right):
-            if item1 > item2:
-                return False
+import json
+
+def compare(left, right):
+    print(left, right)
     return True
-
-def compare_numbers(numbers):
-    num1, num2 = numbers
-    cut1 = num1[0]
-    cut2 = num2[0]
-    while len(cut1) > 0:
-        if isinstance(cut1, int):
-            cut1 = [cut1]
-        if isinstance(cut2, int):
-            cut2 = [cut2]
-        compare_list(cut1, cut2)
-
-
 
 with open("test.txt") as file:
     lines = file.read().split("\n\n")
 
-for line in lines:
-    numbers = [ast.literal_eval(lst) for lst in line.split("\n")]
-    compare_numbers(numbers)
-
-num1= [7,7,7]
-num2= [7,7,7,7]
-print(compare_list(num1, num2))
+check_sum = 0
+for index, line in enumerate(lines, start=1):
+    left, right = [json.loads(json_string) for json_string in line.split("\n")]
+    if compare(left, right):
+        check_sum += index
